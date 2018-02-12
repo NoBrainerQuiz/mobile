866d8e5b4fdb7dba40c9b8cf90c0a46f6d30e7b3
jamesdavy21 <jamesdavy21@hotmail.co.uk>
2018-02-12 11:41:15 +0000

    App drawer created

--- NoBrainer/app/src/main/java/com/example/up804392/nobrainer/HomeActivity.java
+++ NoBrainer/app/src/main/java/com/example/up804392/nobrainer/HomeActivity.java
@@ -1,20 +1,60 @@
 package com.example.up804392.nobrainer;
 
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 
-public class HomeActivity extends AppCompatActivity {
-
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navBar);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
 
     }
 
+    @Override
+    public void onBackPressed() {
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.home)
+            Toast.makeText(this, "Testing", Toast.LENGTH_SHORT).show();
+        if(id == R.id.about) {
+            startActivity(new Intent(HomeActivity.this, HomeActivity.class));
+        }
+        return false;
+    }
 }
