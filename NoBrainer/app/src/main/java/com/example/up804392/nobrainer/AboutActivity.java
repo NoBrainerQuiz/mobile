1fda7bd67902e5561bca0bdbda692829bbb15701
jamesdavy21 <jamesdavy21@hotmail.co.uk>
2018-02-16 11:36:08 +0000

    About page created and linked

--- NoBrainer/app/src/main/java/com/example/up804392/nobrainer/AboutActivity.java
+++ NoBrainer/app/src/main/java/com/example/up804392/nobrainer/AboutActivity.java
@@ -2,26 +2,24 @@ package com.example.up804392.nobrainer;
 
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.PopupMenu;
 import android.widget.Toast;
 
 
-public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class AboutActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_about);
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
@@ -51,9 +49,9 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
         if(id == R.id.home)
-            Toast.makeText(this, "Current page", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(AboutActivity.this, HomeActivity.class));
         if(id == R.id.about) {
-            startActivity(new Intent(HomeActivity.this, AboutActivity.class));
+            Toast.makeText(this, "Current page", Toast.LENGTH_SHORT).show();
         }
         return false;
     }
